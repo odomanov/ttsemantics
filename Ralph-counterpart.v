8b0e6e945757c960c7344a47aaa8c9dc8828b6b0
Oleg Domanov <odomanov@yandex.ru>
2018-07-05 20:32:11 +0700

    Modified   Ralph-counterpart.v

--- Ralph-counterpart.v
+++ Ralph-counterpart.v
@@ -1,14 +1,168 @@
 (** Фраза Куайна "Ральф верит, что кто-то шпион" *)
 (** С использованием counterpart relation        *)
 
-
 (** Сигнатура  **)
+
 Parameter man:Set.
 Parameter spy: man->Prop.
 
 
 
-(** есть контекст ГA, отношение двойников вне контекста  *)
+(** Нет контекста ГA, т.е. контекст оценки ("наш") не обозначен,
+    отношение двойников --- в контексте оценки *)
+
+Module Ralph_counterpart3.
+
+  (** Контекст Ральфа **)
+  Record ГR:Type := mkГR
+    {xh:man;           (* человек в шляпе *)
+     xb:man;           (* человек на пляже *)
+     spy_h: spy(xh);   (* человек в шляпе --- шпион *)
+     spy_b: ~spy(xb)}. (* человек на пляже --- не шпион *)
+
+  (** контекст оценки *)
+
+  Parameter gr:ГR.
+  Parameter o:man.
+
+  (** counterpart relation  *)
+
+  Inductive C (m:man) (mr:ГR->man):Prop :=
+    | cp: C m mr.    (* конструктор отношения *)
+  (* определяем отношение *)
+  Definition coh:= cp o xh.
+  Definition cob:= cp o xb.
+
+  (* типы двойников *)
+  Definition cRinA (x:ГR->man): Type := { m:man | C m x }.
+  Definition cAinR (x:man): Type := { m:ГR->man | C x m }.
+
+  (* Тип cAinR(o) непуст. Доказываем это построением термов *)
+  (* можно было сделать просто: mmh:=exist _ xh coh   *)
+  Lemma mmh:cAinR(o).
+  Proof.
+    unfold cAinR.
+    exists xh.
+    apply coh.
+  Defined.
+
+  Lemma mmb:cAinR(o).
+  Proof.
+    unfold cAinR.
+    exists xb.
+    apply cob.
+  Defined.
+
+  (* среди двойников Орткута есть шпион *)
+  Fact ex: exists m:cAinR(o), forall gr:ГR, spy ((proj1_sig m) gr).
+  Proof.
+    exists mmh.
+    apply spy_h.
+  Qed.
+
+  (* ...и есть НЕ шпион *)
+  Fact exn: exists m:cAinR(o), forall gr:ГR, ~spy ((proj1_sig m) gr).
+  Proof.
+    exists mmb.
+    apply spy_b.
+  Qed.
+
+
+  (** ********************************************************************)
+  (**     de dicto      *)
+  (** ********************************************************************)
+
+  Fact spyh: forall gr:ГR, spy (xh gr).
+  Proof.
+    apply spy_h.
+  Qed.
+
+  Fact spyb: forall gr:ГR, ~spy (xb gr).
+  Proof.
+    apply spy_b.
+  Qed.
+
+  (* Ральф верит, что существует кто-то... *)
+  Fact spyh_ex: forall gr:ГR, exists m:ГR->man, spy (m gr).
+  Proof.
+    exists xh.
+    apply spy_h.
+  Qed.
+
+  Fact spyb_ex: forall gr:ГR, exists m:ГR->man, ~spy (m gr).
+  Proof.
+    exists xb.
+    apply spy_b.
+  Qed.
+
+  
+  (** ********************************************************************)
+  (**     de re         *)
+  (** ********************************************************************)
+
+  (* существует чел. в нашем контексте, такой, что существует его двойник,
+     о котором Ральф верит, что он --- шпион  *)
+  Fact exh: exists ma:man, exists mc:cAinR(ma), 
+    forall gr:ГR, spy ((proj1_sig mc) gr).
+  Proof.
+    exists o.
+    exists mmh.
+    apply spy_h.
+  Qed.
+
+  (* существует чел. в нашем контексте, такой, что существует его двойник,
+     о котором Ральф верит, что он --- НЕ шпион  *)
+  Fact exb: exists ma:man, exists mc:cAinR(ma), 
+    forall gr:ГR, ~spy ((proj1_sig mc) gr).
+  Proof.
+    exists o.
+    exists mmb.
+    apply spy_b.
+  Qed.
+
+  (* Мы верим, что Ральф верит, что Орткут шпион  *)
+  Fact oh: exists mc:cAinR(o), 
+    forall gr:ГR, spy ((proj1_sig mc) gr).
+  Proof. exists mmh. apply spy_h. Qed.
+
+  (* Мы верим, что Ральф НЕ верит, что Орткут шпион  *)
+  Fact ob: exists mc:cAinR(o), 
+    forall gr:ГR, ~spy ((proj1_sig mc) gr).
+  Proof. exists mmb. apply spy_b. Qed.
+
+  (** Общие объекты *)
+  Record mC: Type := mkmC
+    {mCm: man;
+     mCr: ГR->man;
+     cCR: C mCm mCr}.
+
+  Fact ospy: exists mc:mC, 
+    o = mCm mc /\ forall gr:ГR, spy(mCr mc gr).
+  Proof.
+    exists (mkmC o xh coh).
+    split.
+    auto.
+    apply spy_h.
+  Qed.
+
+  Fact ospyn: exists mc:mC, 
+    o = mCm mc /\ forall gr:ГR, ~spy(mCr mc gr).
+  Proof.
+    exists (mkmC o xb cob).
+    split.
+    auto.
+    apply spy_b.
+  Qed.
+
+
+
+End Ralph_counterpart3.
+
+
+
+
+
+(** есть контекст ГA, отношение двойников --- вне контекста  *)
 
 Module Ralph_counterpart.
 
@@ -25,10 +179,10 @@ Module Ralph_counterpart.
 
   (** counterpart relation  *)
 
-  Definition Cpart:= (ГA->man) -> (ГR->man) -> Prop.
-  Inductive C : Cpart :=
-    | coh : C o xh
-    | cob : C o xb.
+  Inductive C (ma:ГA->man) (mr:ГR->man): Prop :=
+  | cp: C ma mr.
+  Definition coh := cp o xh.
+  Definition cob := cp o xb.
 
   (* типы двойников *)
   Definition cinA (x:ГR->man): Type := { m:ГA->man | C m x }.
@@ -59,7 +213,7 @@ Module Ralph_counterpart.
     apply spy_h.
   Qed.
 
-  (* и есть НЕ шпион *)
+  (* ...и есть НЕ шпион *)
   Fact exn: exists m:cinR(o), forall gr:ГR, ~spy ((proj1_sig m) gr).
   Proof.
     intros.
@@ -166,7 +320,8 @@ End Ralph_counterpart.
 
 
 
-(** есть контекст ГA, отношение двойников в контексте (контекст оценки) *)
+(** есть контекст ГA, отношение двойников --- в этом контексте 
+    (контексте оценки) *)
 
 Module Ralph_counterpart2.
 
@@ -177,16 +332,12 @@ Module Ralph_counterpart2.
      spy_h: spy(xh);   (* человек в шляпе --- шпион *)
      spy_b: ~spy(xb)}. (* человек на пляже --- не шпион *)
 
-  (** counterpart relation  *)
-
-  Definition Cpart:= man -> (ГR->man) -> Prop.
-
   (** контекст оценки *)
 
   Record ГA:Type := mkГA
     {gr:ГR;
      o:man;
-     C:Cpart;
+     C: man->(ГR->man)->Prop;
      coh: C o xh;
      cob: C o xb}.
 
@@ -303,127 +454,3 @@ Module Ralph_counterpart2.
   Qed.
 
 End Ralph_counterpart2.
-
-
-
-
-
-(** Нет контекста ГA  *)
-
-Module Ralph_counterpart3.
-
-  (** Контекст Ральфа **)
-  Record ГR:Type := mkГR
-    {xh:man;           (* человек в шляпе *)
-     xb:man;           (* человек на пляже *)
-     spy_h: spy(xh);   (* человек в шляпе --- шпион *)
-     spy_b: ~spy(xb)}. (* человек на пляже --- не шпион *)
-
-  (** counterpart relation  *)
-
-  Definition Cpart:= man -> (ГR->man) -> Prop.
-
-  (** контекст оценки *)
-
-  Parameter gr:ГR.
-  Parameter o:man.
-  Parameter C:Cpart.
-  Parameter coh: C o xh.
-  Parameter cob: C o xb.
-
-  (* типы двойников *)
-  Definition cRinA (x:ГR->man): Type := { m:man | C m x }.
-  Definition cAinR (x:man): Type := { m:ГR->man | C x m }.
-
-  (* Тип cAinR(o) непуст. Доказываем это построением термов *)
-  (* можно было сделать просто: mmh:=exist _ xh coh   *)
-  Lemma mmh:cAinR(o).
-  Proof.
-    unfold cAinR.
-    exists xh.
-    apply coh.
-  Defined.
-
-  Lemma mmb:cAinR(o).
-  Proof.
-    unfold cAinR.
-    exists xb.
-    apply cob.
-  Defined.
-
-  (* среди двойников Орткута есть шпион *)
-  Fact ex: exists m:cAinR(o), forall gr:ГR, spy ((proj1_sig m) gr).
-  Proof.
-    exists mmh.
-    apply spy_h.
-  Qed.
-
-  (* и есть НЕ шпион *)
-  Fact exn: exists m:cAinR(o), forall gr:ГR, ~spy ((proj1_sig m) gr).
-  Proof.
-    exists mmb.
-    apply spy_b.
-  Qed.
-
-
-  (** ********************************************************************)
-  (**     de re         *)
-  (** ********************************************************************)
-
-  (* существует чел. в нашем контексте, такой, что существует его двойник,
-     о котором Ральф верит, что он --- шпион  *)
-  Fact exh: exists ma:man, exists mc:cAinR(ma), 
-    forall gr:ГR, spy ((proj1_sig mc) gr).
-  Proof.
-    exists o.
-    exists mmh.
-    apply spy_h.
-  Qed.
-
-  (* существует чел. в нашем контексте, такой, что существует его двойник,
-     о котором Ральф верит, что он --- НЕ шпион  *)
-  Fact exb: exists ma:man, exists mc:cAinR(ma), 
-    forall gr:ГR, ~spy ((proj1_sig mc) gr).
-  Proof.
-    exists o.
-    exists mmb.
-    apply spy_b.
-  Qed.
-
-  (* Мы верим, что Ральф верит, что Орткут шпион  *)
-  Fact oh: exists mc:cAinR(o), 
-    forall gr:ГR, spy ((proj1_sig mc) gr).
-  Proof. exists mmh. apply spy_h. Qed.
-
-  (* Мы верим, что Ральф НЕ верит, что Орткут шпион  *)
-  Fact ob: exists mc:cAinR(o), 
-    forall gr:ГR, ~spy ((proj1_sig mc) gr).
-  Proof. exists mmb. apply spy_b. Qed.
-
-  (** Общие объекты *)
-  Record mC: Type := mkmC
-    {mCm: man;
-     mCr: ГR->man;
-     cCR: C mCm mCr}.
-
-  Fact ospy: exists mc:mC, 
-    o = mCm mc /\ forall gr:ГR, spy(mCr mc gr).
-  Proof.
-    exists (mkmC o xh coh).
-    split.
-    auto.
-    apply spy_h.
-  Qed.
-
-  Fact ospyn: exists mc:mC, 
-    o = mCm mc /\ forall gr:ГR, ~spy(mCr mc gr).
-  Proof.
-    exists (mkmC o xb cob).
-    split.
-    auto.
-    apply spy_b.
-  Qed.
-
-
-
-End Ralph_counterpart3.
